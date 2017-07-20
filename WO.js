e742dca7d3e929ad2f598aaf24b0cd0ad993aa1b
barndawg <barneyswatson@gmail.com>
2017-07-20 18:53:28 +0100

    Version 0.5c
    - Changed stylesheet to use Roboto Mono as body font
    - Added UI for 3 new WIP features - ADSR, LFO, Sequencer. None of them
    work yet.
    - Volume control now actually works! Logarithmic volume scaling.
    - Ability to turn off console logs (actually added this in 0.5a but
    forgot about it)
    - Massive under-the-hood additions with a function which converts note
    names into frequencies. This will be integral in the implementation of
    the sequencer.

--- WO.js
+++ WO.js
@@ -22,7 +22,7 @@ function play(){
 
     if (checked){
       console.log('[PLAY] Oscillator ' + num + ' is on. Getting other inputs...');
-      var freq = document.getElementById('freq').value; // Get initial frequency
+      var freq = 440; // Get initial frequency
       var play = document.getElementById('freq' + num).value; // Get value of freq offset
       var type = document.getElementById('type' + num).value; // Get value of waveform
       var uni = document.getElementById('uni' + num).checked; // Get value of unison
@@ -61,6 +61,7 @@ function stop(){
 
 function flow(i){
   var path = document.getElementById('filters').value; // Get value of filter select
+  var adsr = createVolumeEnvelope();
 
   if (path == 'none'){
     console.log('[FLOW] Signal path = osc -> vol -> out. Setting...');
@@ -83,6 +84,13 @@ function flow(i){
   volume.connect(context.destination); // Connect volume control to audio out (out)
 }
 
+function adsr(i){
+  var att = document.getElementById('att'),
+    dec = document.getElementById('dec'),
+    sust = document.getElementById('sust'),
+    rel = document.getElementById('rel');
+}
+
 function createFilter(type, cutoff){
   console.log('[CREATEFILTER] Creating biquad filter with type ' + type + ' and cutoff ' + cutoff + 'hZ...');
   var filter = context.createBiquadFilter(); // Create biquad filter
@@ -94,7 +102,79 @@ function createFilter(type, cutoff){
   return num; // Return filter position
 }
 
-var ver = '0.5a';
+function createVolumeEnvelope(gain = 1){
+  console.log('[CREATEVOLUMEENVELOPE] Creating volume envelope with gain' + gain + '...');
+  var envelope = context.createGain(); // Create gain node for volume control
+  envelope.gain.value = 1; // Set volume to 1;
+  envs.push(envelope); // Push envelope to array
+  var num = envs.length - 1; // Get envelope position
+  console.log('[CREATEVOLUMEENVELOPE] Filter position in array is ' + num.toString() + '.');
+  return num; // Return envelope position
+}
+
+function noteToFrequency(note = 'C5'){
+  console.log('[NOTETOFREQUENCY] Converting note ' + note + ' to frequency in hZ...');
+  var notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'], // List of all notes in 1 octave (enharmonics not included)
+    noteName, noteOct, // For if statements later
+    octDiff, singleOctSteps, steps, // For step calculations
+    root = Math.pow(2, 1/12); // 12th root of 2 for conversion
+
+  // Processes the note input into a note name and an octave. If it is passed an incorrect value it outputs C5.
+  if (!isNaN(parseInt(note.substring(1)))){
+    noteName = note.charAt(0); // Normal note (2nd character+ is octave)
+  } else if (note.charAt(1) === '#' || note.charAt(1) === 'b') {
+    noteName = note.substring(0, 2); // Note with accidental (3rd character+ is octave)
+  } else {
+    console.log('[NOTETOFREQUENCY] Invalid note name. Setting name to C.');
+    noteName = 'C'; // Not a valid note
+  }
+
+  if (noteName.charAt(1) === 'b'){
+    noteName = notes[notes.indexOf(noteName.charAt(0)) - 1]; // Deal with flats here
+  }
+
+  if (!notes.includes(noteName)){
+    console.log('[NOTETOFREQUENCY] Invalid note name. Setting name to C.');
+    noteName = 'C'; // Not a valid note
+  }
+
+  if (noteName.length === 1){
+    noteOct = parseInt(note.substring(1)); // Normal note
+  } else {
+    noteOct = parseInt(note.substring(2)); // Note with accidental
+  }
+
+  if (isNaN(noteOct)){
+    console.log('[NOTETOFREQUENCY] Invalid note octave. Setting octave to 5.');
+    noteOct = 5; // Not a valid octave
+  }
+
+  // Figures out the number of semitones between it and A4
+  // +-12st for octave difference
+  octDiff = noteOct - 4; // + if octave is greater, - if it is less
+  singleOctSteps = notes.indexOf(noteName.charAt(0)) - notes.indexOf('A'); // Gets steps in 1 oct from A, not counting accidentals
+
+  if (noteName.charAt(1) === '#'){
+    singleOctSteps = singleOctSteps + 1; // Add one step for sharp
+  }
+
+  if (octDiff < 0){
+    steps = singleOctSteps + 12 * (octDiff); // Step difference with supplemental octaves
+  } else if (octDiff > 0) {
+    steps = singleOctSteps + 12 * (octDiff); // Step difference with positive octaves
+  } else {
+    steps = singleOctSteps;
+  }
+  console.log(steps);
+
+  console.log(isNaN(steps));
+
+  var freq = 440 * Math.pow(root, steps);
+  console.log('[NOTETOFREQUENCY] Finished converting! Frequency for ' + note + ' is ' + freq + '.');
+  return freq;
+}
+
+var ver = '0.5c';
 var log = true;
 
 console.out = console.log.bind(console); // Make console.out = normal console.log
@@ -110,13 +190,22 @@ console.log('[INIT] Welcome to WØ-VST v' + ver + '!');
 console.log('[INIT] Creating oscillator, filter, and lfo arrays...');
 var oscs = []; // Create oscillator array
 var filts = []; // Create filter array
+var envs = []; // Create envelope array
 
 console.log('[INIT] Creating audio context...');
 var context = new AudioContext(); // Create audio context
 
-console.log('[INIT] Creating gain node for volume control...');
-var volume = context.createGain(); // Create gain node for volume control
-volume.gain.value = 0.5; // Set volume to 0.5;
+console.log('[INIT] Creating envelope for volume control...');
+var volume = envs[ createVolumeEnvelope(1) ]; // Create gain node for volume control
+var volumeControl = document.getElementById('vol'); // Get volume control element
+volumeControl.addEventListener('input', function(){
+  if (volumeControl.value > 100){
+    volumeControl.value = 100;
+  } else if (volumeControl.value < 0){
+    volumeControl.value = 0;
+  }
+  volume.gain.value = (Math.exp(volumeControl.value / 100) - 1) / (Math.E - 1); // Logarithmic volume control
+});
 
 console.log('[INIT] Creating biquad filters...');
 var hpf = createFilter('highpass', 660); // Create highpass filter
